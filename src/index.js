3476788eba25319a666f079742d2af80cc13892b
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-03-26 00:22:18 +0300

    comleted assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const button = document.getElementsByTagName('input')[1]
+
+  button.onclick = function(){
+    let newTask =document.getElementById("new-task-description").value;
+
+    document.getElementById("tasks").innerHTML +=
+    ' <li> ${newTask}  <button id="clearTask" >x<button> </li>';
+    let done = document.getElementById("clear");
+
+    for (let i = 0; i < done; i++){
+      done[i].parentNode.style.removeElement()
+    }
+  }
+}
+
+);
